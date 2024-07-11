a2aa4e6c213e1d2396984cd7654269bbec31b7bf
YagnikManav <yagnik.c@ahduni.edu.in>
2024-07-11 09:38:20 +0530

    all filed added

--- src/components/Preface.jsx
+++ src/components/Preface.jsx
@@ -3,29 +3,18 @@ import TransformationGSAP from './ui/TransformationGSAP';
 import backgroundImage from '/heroBg.png';
 import {motion} from 'framer-motion'
 import { fadeIn } from '../utils/motion';
-import {useInView} from 'react-intersection-observer';
-
 
 
 const Preface = () => {
-  const [ref, inView] =useInView({triggerOnce: true})
-  const [imageIndex, setImageIndex] = useState(0);
-  const images =[
-    './visionPro.png',
-    './iphone.png',
-    './computer.png'
-  ]
 
   const [animationComplete, setAnimationComplete] = useState(false);
 
   const handleAnimationComplete = () => {
-    setImageIndex((prevIndex) =>(prevIndex + 1) % images.length);
+    setAnimationComplete(true);
   };
 
   return (
-    <div >
-
-  <motion.section style={{ backgroundImage: `url(${backgroundImage})` }} className=' w-full  min-h-full preface-section bg-[#161a30] max-lg:px-4 py-12 px-40'>
+    <motion.section style={{ backgroundImage: `url(${backgroundImage})` }} className=' w-full  min-h-full preface-section bg-[#161a30] max-lg:px-4 py-12 px-40'>
 
       <div className='max-lg:text-center max-lg:gap-2 max-lg:flex-col flex flex-row items-center justify-between min-h-full'>
         <div className='max-lg:flex max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:mx-auto w-full'>
@@ -54,11 +43,9 @@ const Preface = () => {
         whileInView="show"
         onAnimationComplete={handleAnimationComplete} 
 
-        className='w-[30%] max-lg:w-full ' src={images[imageIndex]}/>
+        className='w-[30%] max-lg:w-full ' src='./prefaceOne.png'/>
       </div>
     </motion.section>
-  
-    </div>
   );
 }
 
